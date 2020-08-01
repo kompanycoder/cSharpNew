f85130bc4b6268e59c18ea7cd860b713c08c4068
kompanycoder <mwegagideon@gmail.com>
2020-08-01 14:26:05 +0300

    practice with numbers and area successful.

--- Program.cs
+++ Program.cs
@@ -4,10 +4,33 @@ namespace myApp
 {
     class Program
     {
+        static void executeNumbers()
+        {
+            Console.WriteLine("Begining of program execution");
+            int a = 15;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine("The Sum is : " + c);
+            c = b - a;
+            Console.WriteLine("The Subtraction difference is : " + c);
+            c = a * b;
+            Console.WriteLine("The Multiplication is : " + c);
+            c = a / b;
+            Console.WriteLine("The Division result is : " + c);
+            Console.WriteLine("End of Program execution");
+        }
+
+        static void Area()
+        {
+            double PI = Math.PI;
+            double r = Math.Floor(Math.Abs(7.0));
+            double area = PI * r * r;
+            Console.WriteLine("The Area is : " + area);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current Time now is: " + DateTime.Now);
+            executeNumbers();
+            Area();
         }
     }
 }
